e91f898f01b8ac3e716e89ba07823acffbc99b5a
zly <zly@aaa.com>
2017-05-04 19:24:22 +0800

    modify some about pom.xml to refractor

--- sprssoLibCommon/src/main/java/sprssozdemo1/libCommon/util/Util1.java
+++ sprssoLibCommon/src/main/java/sprssozdemo1/libCommon/util/Util1.java
@@ -37,7 +37,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.header.writers.frameoptions.StaticAllowFromStrategy;
+
 
 
 
